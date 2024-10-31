65f6e3156b421ba92bf388050d7c017e6238fc3a
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2024-10-30 19:02:10 -0500

    DONE!!!

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ let rightBtn = document.getElementById("rightBtn");
 
 const images = [
     'images/dinner.png',
-    'images/freakbob.png',
-    'images/heidicartman.png'
-    ];
+    'images/freakbob.jpg',
+    'images/heidicartman.jpg'
+];
 
 qwelPicture.forEach(function (image, index) {
     image.addEventListener("click", function () {
@@ -32,7 +32,7 @@ qwelPicture.forEach(function (image, index) {
     })
 });
 
-leftBtn.addEventListener("click", function() {
+leftBtn.addEventListener("click", function () {
     if (qwelCurrentIndex > 0) {
         qwelCurrentIndex--;
         qwelPlaceHolderImg.src = images[qwelCurrentIndex];
@@ -43,12 +43,48 @@ leftBtn.addEventListener("click", function() {
 });
 
 
-rightBtn.addEventListener("click", function() {
+rightBtn.addEventListener("click", function () {
     if (qwelCurrentIndex < images.length - 1) {
         qwelCurrentIndex++;
         qwelPlaceHolderImg.src = images[qwelCurrentIndex];
     } else {
         qwelCurrentIndex = 0;
         qwelPlaceHolderImg.src = images[qwelCurrentIndex];
+    }
+});
+
+// move cat
+const moveCat = document.getElementById("movecat");
+var count = 0;
+const form = "<a href=\"https://forms.gle/nknQQvWBLdUcNTvXA\" target=\"_blank\">form</a>"
+moveCat.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (window.innerWidth - 112) + 56);
+    const j = Math.floor(Math.random() * (window.innerHeight - 112) + 56);
+    moveCat.style.left = i + 'px';
+    moveCat.style.top = j + 'px';
+    count += 1;
+
+    // msg update lol
+    const boredMsg = document.querySelector(".qwelTxt");
+    if (count > 0 && count <= 5) {
+        boredMsg.innerHTML = "Yoo, isn't this kinda fun. You should keep trying to catch the cat";
+        boredMsg.style.fontSize = "medium";
+        moveCat.style.width = "6rem";
+        moveCat.style.height = "6rem";
+    } else if (count > 5 && count <= 10) {
+        boredMsg.innerHTML = "Damn you're almost there. Keep going.";
+        boredMsg.style.fontSize = "large";
+        moveCat.style.width = "5rem";
+        moveCat.style.height = "5rem";
+    } else if (count > 10 && count <= 20) {
+        boredMsg.innerHTML = "Almostttttt.";
+        boredMsg.style.fontSize = "x-large";
+        moveCat.style.width = "4rem";
+        moveCat.style.height = "4rem";
+    } else if (count > 20) {
+        boredMsg.innerHTML = "Yay, you just spent more time rotting! You're definitely number one (out of the 10 Camilles). Please fill out this " + form + ".";
+        boredMsg.style.fontSize = "xx-large";
+        moveCat.style.width = "7rem";
+        moveCat.style.height = "7rem";
     }
 });
